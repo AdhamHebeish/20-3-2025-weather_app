187b0a9e7b07a9bf7bbe3c00da021ba15c6d37a5
Adham Hebeish <adham.hebeish1@gmail.com>
2025-03-29 03:16:46 +0100

    add weather apis

---
+++ weather_app/api/weather_api.py
@@ -0,0 +1,41 @@
+import frappe
+import requests
+
+@frappe.whitelist(allow_guest=True)
+def search(q):
+    url = "http://api.weatherapi.com/v1/search.json"
+    args = {
+        "key": "4ceacd128ade4af3bd2190728252203",
+        "q": q
+    }
+    response = requests.get(url, params=args)
+    return response.json()
+
+@frappe.whitelist(allow_guest=True)
+def newlocalStorage(city,region,country,lat,lon,api):
+    try:
+        memoryitem = frappe.new_doc("localStorage")
+        memoryitem.city = city
+        memoryitem.region = region
+        memoryitem.country = country
+        memoryitem.lat = lat
+        memoryitem.lon = lon
+        memoryitem.api = api
+        memoryitem.insert(ignore_permissions=True)
+        frappe.db.commit()
+        status_code = 201
+        return status_code
+    except Exception as err:
+        status_code = 500
+        return status_code,err
+
+@frappe.whitelist(allow_guest=True)
+def current(q, aqi):
+    url = "http://api.weatherapi.com/v1/current.json"
+    args = {
+        "key": "4ceacd128ade4af3bd2190728252203",
+        "q": q,
+        "aqi" : aqi
+    }
+    response = requests.get(url, params=args)
+    return response.json()
--- weather_app/weather_app/doctype/localstorage/localstorage.py
+++ weather_app/weather_app/doctype/localstorage/localstorage.py
@@ -6,4 +6,19 @@ from frappe.model.document import Document
 
 
 class localStorage(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		api: DF.Data
+		city: DF.Data
+		country: DF.Data
+		lat: DF.Float
+		lon: DF.Float
+		region: DF.Data | None
+	# end: auto-generated types
 	pass
